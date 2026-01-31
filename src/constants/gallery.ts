aa8b73b25dbe514ee5f8e65ae5398a1d8fd8886c
Sameen-K-A <sameensameen60@gmail.com>
2026-02-01 01:58:54 +0530

    Spelling mistake

--- src/constants/gallery.ts
+++ src/constants/gallery.ts
@@ -25,7 +25,7 @@ export const TEAM_MEMBERS: TeamMember[] = [
   { img: "/images/about/team/AjithKumar.jpg", name: "Ajith Kumar", role: "Business Development Manager", id: 1 },
   { img: "/images/about/team/AjithPalakkada.jpg", name: "Ajith Palakkada", role: "Operations Manager", id: 2 },
   { img: "/images/about/team/AnudeepMukundan.jpg", name: "Anudeep Mukundan", role: "Sales Manager", id: 3 },
-  { img: "/images/about/team/MidhunTM.jpg", name: "Midhun T M", role: "Team Leader", id: 4 },
+  { img: "/images/about/team/MidhunTM.jpg", name: "Midhun T M", role: "Operations Team Leader", id: 4 },
   { img: "/images/about/team/AbhithThirumeni.jpg", name: "Abhith Thirumeni", role: "Strategic Partner", id: 5 },
   { img: "/images/about/team/RithujithMohan.jpg", name: "Rithujith Mohan", role: "Operations Executive", id: 6 },
 
